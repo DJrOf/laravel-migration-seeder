8c2d86a3ebee6f78dd515122a3076e3e436efe02
DJrOf <danieljrofosu@gmail.com>
2022-04-01 09:04:43 +0200

    to fix e finish

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    //
+}
+
+public function index() {
+
+    $data = array
+
+    return view('welcome')
+}
